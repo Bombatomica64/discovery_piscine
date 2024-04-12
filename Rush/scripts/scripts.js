38415a6b00893b7053752145e09f7cac6065e2a4
Bombatomica_64 <qualcosa2001@gmail.com>
2024-04-12 12:20:38 +0200

    Remove unused CSS and JavaScript files

--- Rush/scripts/scripts.js
+++
@@ -1,4 +0,0 @@
-$('.dropdown-menu a').on('click', function(e) {
-    e.preventDefault();
-    console.log('You clicked on ' + $(this).text());
-});
---
+++ Rush/scripts/typed.js
@@ -0,0 +1,11 @@
+$('.dropdown-menu a').on('click', function(e) {
+    e.preventDefault();
+    console.log('You clicked on ' + $(this).text());
+});
+
+var typed = new Typed('#animated-text', {
+    strings: ["This is the animated text."],
+    typeSpeed: 500,
+    backSpeed: 50,
+    loop: true
+});
